7a003c7d67a64e64c8f10aa67d6ac8e7f9b0a179
Waldemar Hummer <waldemar.hummer@gmail.com>
2024-03-23 23:44:09 +0100

    add initial configuration to run sample in LocalStack (#1)

--- connector.py
+++ connector.py
@@ -26,6 +26,7 @@ def connect():
         warehouse=config.SNOWFLAKE_WAREHOUSE,
         schema=config.SNOWFLAKE_SCHEMA,
         database=config.SNOWFLAKE_DATABASE,
+        host=config.SNOWFLAKE_HOST,
         private_key=pkb,
         session_parameters={
             'QUERY_TAG': 'Snowflake-Python-Connector',
@@ -49,9 +50,9 @@ def get_trips_monthly():
     start_range = request.args.get('start_range')
     end_range = request.args.get('end_range')
     if start_range and end_range and params_valid(start_range, end_range):
-        sql = f"select COUNT(*) as trip_count, MONTHNAME(starttime) as month from demo.trips where starttime between '{start_range}' and '{end_range}' group by MONTH(starttime), MONTHNAME(starttime) order by MONTH(starttime);"
+        sql = f"select COUNT(*) as trip_count, MONTHNAME(starttime) as month from trips where starttime between '{start_range}' and '{end_range}' group by MONTH(starttime), MONTHNAME(starttime) order by MONTH(starttime);"
         return exec_and_fetch(sql)
-    sql = "select COUNT(*) as trip_count, MONTHNAME(starttime) as month from demo.trips group by MONTH(starttime), MONTHNAME(starttime) order by MONTH(starttime);"
+    sql = "select COUNT(*) as trip_count, MONTHNAME(starttime) as month from trips group by MONTH(starttime), MONTHNAME(starttime) order by MONTH(starttime);"
     return exec_and_fetch(sql)
 
 
@@ -61,9 +62,9 @@ def get_day_of_week():
     start_range = request.args.get('start_range')
     end_range = request.args.get('end_range')
     if start_range and end_range and params_valid(start_range, end_range):
-        sql = f"select COUNT(*) as trip_count, DAYNAME(starttime) as day_of_week from demo.trips where starttime between '{start_range}' and '{end_range}' group by DAYOFWEEK(starttime), DAYNAME(starttime) order by DAYOFWEEK(starttime);"
+        sql = f"select COUNT(*) as trip_count, DAYNAME(starttime) as day_of_week from trips where starttime between '{start_range}' and '{end_range}' group by DAYOFWEEK(starttime), DAYNAME(starttime) order by DAYOFWEEK(starttime);"
         return exec_and_fetch(sql)
-    sql = "select COUNT(*) as trip_count, DAYNAME(starttime) as day_of_week from demo.trips group by DAYOFWEEK(starttime), DAYNAME(starttime) order by DAYOFWEEK(starttime);"
+    sql = "select COUNT(*) as trip_count, DAYNAME(starttime) as day_of_week from trips group by DAYOFWEEK(starttime), DAYNAME(starttime) order by DAYOFWEEK(starttime);"
     return exec_and_fetch(sql)
 
 
@@ -73,7 +74,7 @@ def get_temperature():
     start_range = request.args.get('start_range')
     end_range = request.args.get('end_range')
     if start_range and end_range and params_valid(start_range, end_range):
-        sql = f"with weather_trips as (select * from demo.trips t inner join demo.weather w on date_trunc(\"day\", t.starttime) = w.observation_date) select round(temp_avg_f, -1) as temp, count(*) as trip_count from weather_trips where starttime between '{start_range}' and '{end_range}' group by round(temp_avg_f, -1) order by round(temp_avg_f, -1) asc;"
+        sql = f"with weather_trips as (select * from trips t inner join weather w on date_trunc(\"day\", t.starttime) = w.observation_date) select round(temp_avg_f, -1) as temp, count(*) as trip_count from weather_trips where starttime between '{start_range}' and '{end_range}' group by round(temp_avg_f, -1) order by round(temp_avg_f, -1) asc;"
         return exec_and_fetch(sql)
-    sql = "with weather_trips as (select * from demo.trips t inner join demo.weather w on date_trunc(\"day\", t.starttime) = w.observation_date) select round(temp_avg_f, -1) as temp, count(*) as trip_count from weather_trips group by round(temp_avg_f, -1) order by round(temp_avg_f, -1) asc;"
+    sql = "with weather_trips as (select * from trips t inner join weather w on date_trunc(\"day\", t.starttime) = w.observation_date) select round(temp_avg_f, -1) as temp, count(*) as trip_count from weather_trips group by round(temp_avg_f, -1) order by round(temp_avg_f, -1) asc;"
     return exec_and_fetch(sql)
